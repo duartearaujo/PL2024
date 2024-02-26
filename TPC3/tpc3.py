62377f2c11f028ba1df6a6f1bf851b77993ae093
a100750 <a100750@uminho.pt>
2024-02-26 12:44:27 +0000

    update tpc3

--- TPC3/tpc3.py
+++ TPC3/tpc3.py
@@ -4,12 +4,12 @@ import re
 stdin = sys.stdin.read()
 on = False
 sum = 0
-for match in re.finditer(r'(([\+\-]?\d+)|([Oo][Nn])|([Oo][Ff]{2})|=)', stdin):
-    if match.group(1) == match.group(3) and not on:
+for match in re.finditer(r'([\+\-]?\d+|on|off|=)', stdin, re.IGNORECASE):
+    if match.group(1).lower() == 'on' and not on:
         on = True
-    elif match.group(1) == match.group(4):
+    elif match.group(1).lower() == 'off':
         on = False
-    elif match.group(1) == match.group(2) and on:
-        sum += int(match.group(2))
-    elif match.group(1) == "=":
+    elif match.group(1) == '=':
         print(sum)
+    elif on:
+        sum += int(match.group(1))
